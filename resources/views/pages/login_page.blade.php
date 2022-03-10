ea7499cbc888bb22f03afd6df67b74ff0489d048
Evgeny <puls2007@rambler.ru>
2022-03-11 01:30:33 +0500

    13: Исправить шаблоны

--- resources/views/pages/login_page.blade.php
+++ resources/views/pages/login_page.blade.php
@@ -1,26 +1,34 @@
 @extends('layout.index')
 
 @section('content')
-    <div class="form-register px-6 py-4 max-w-6xl">
-        <form action="{{ route('login') }}" method="POST" class="grid grid-cols-1">
-            @csrf
-            <div class="form-register__wrapper px-6 py-4">
-                @foreach ($errors->all() as $error)
-                    <div class="alert alert-danger">{{ $error }}</div>
-                @endforeach
-            </div>
+    <div class="container">
+        <div class="form-register px-6 py-4 max-w-6xl">
+            <form action="{{ route('login') }}" method="POST" class="grid grid-cols-1">
+                @csrf
+                <div class="form-register__wrapper px-6 py-4">
+                    @foreach ($errors->all() as $error)
+                        <div class="alert alert-danger">{{ $error }}</div>
+                    @endforeach
+                </div>
 
-            <div class="form-register__wrapper px-6 py-4">
-                <input type="text" name="email" placeholder="e-mail адрес" class="h-8" value="{{old('email')}}">
-            </div>
+                <div class="form-register__wrapper px-6 py-4">
+                    <input type="text" name="email" placeholder="e-mail адрес" class="h-8" value="{{old('email')}}">
+                </div>
 
-            <div class="form-register__wrapper px-6 py-4">
-                <input type="password" name="password" placeholder="пароль" class="h-8">
-            </div>
+                <div class="form-register__wrapper px-6 py-4">
+                    <input type="password" name="password" placeholder="пароль" class="h-8">
+                </div>
 
-            <div class="form-register__wrapper px-6 py-4">
-                <button>войти</button>
-            </div>
-        </form>
+                <div class="form-register__wrapper px-6 py-4">
+                    <button>войти</button>
+                </div>
+
+                @if (Route::has('register'))
+                    <div class="form-register__wrapper px-6 py-4">
+                        <a href="{{ route('register') }}">регистрация</a>
+                    </div>
+                @endif
+            </form>
+        </div>
     </div>
 @endsection
--- resources/views/partials/auth_layout.blade.php
+++ resources/views/partials/auth_layout.blade.php
@@ -1,12 +1,12 @@
-<div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-    <a href="{{ route('home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+<div class="header__personal">
+    <a href="#" class="header__cart">
+        <img src="{{ asset('/images/svg/cart.svg') }}" alt="cart" width="50" height="50">
+    </a>
     @auth
-        <a href="{{ route('logout') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Log out</a>
+        <a href="{{ route('logout') }}">Log out</a>
     @else
-        <a href="{{ route('login') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-        @endif
+        <a href="{{ route('login') }}">
+            <img src="{{ asset('/images/svg/login.svg') }}" alt="login" width="41" height="41">
+        </a>
     @endauth
 </div>
