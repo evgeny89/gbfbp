8cde59aeb522e4e0d1c0c275f065bb74ec6aae60
Евгений <52931112+evgeny89@users.noreply.github.com>
2022-03-11 01:40:20 +0500

    13: header layout (#10)
    * 13: Добавить изображения для верстки шапки сайта
    
    * 13: Исправить шаблоны
    
    * 13: Добавить сетку и стили для шапки
    
    * 13: Исправить шрифт
    
    * 13: Добавить копирование картинок при сборке.
    
    * 13: Сборка фронта
    
    Co-authored-by: Evgeny <puls2007@rambler.ru>

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,3 +1,17 @@
-@if (Route::has('login'))
-    @include('partials.auth_layout')
-@endif
+<header class="header">
+    <div class="container">
+        <div class="header-wrapper">
+            <button class="header__btn-menu">
+                <img src="{{ asset('/images/svg/menu.svg') }}" alt="burger" width="83" height="75">
+            </button>
+            <a href="{{ route('home') }}" class="header__logo">
+                <p class="header__tagline">продавайте на hand made</p>
+                <img src="{{ asset('/images/logo.png') }}" alt="logo" width="313" height="50">
+            </a>
+            <input type="text" class="header__search" placeholder="я ищу...">
+            @if (Route::has('login'))
+                @include('partials.auth_layout')
+            @endif
+        </div>
+    </div>
+</header>
