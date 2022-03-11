1600d02f6c6fd90e1c9f0bce6e870ac5a9b76e94
VlaSko1 <vizion84@yandex.ru>
2022-03-11 16:31:56 +0300

    Добавить форму регистрации, стили для формы регистрации, добавил в .gitignore файлы с расширением .lock

---
+++ resources/views/forms/register.blade.php
@@ -0,0 +1,12 @@
+<article class="register-block">
+  <h3 class="register-block__title">Регистрация</h3>
+  <form action="#" class="register-form">
+    <input type="text" name="name" placeholder="name" class="register-form__input">
+    <input type="text" name="email" placeholder="e-mail адрес" class="register-form__input">
+    <input type="password" name="password" placeholder="пароль" class="register-form__input">
+    <input type="password" name="password_confirmation" placeholder="подтвердите пароль" class="register-form__input">
+    <button name="submit" type="submit" class="register-form__button">
+      зарегистрироваться
+    </button>
+  </form>
+</article>
