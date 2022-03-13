47abac28967e9ee76deddee3a0e9f50b9fbda158
VlaSko1 <Vizion84@yandex.ru>
2022-03-13 14:28:53 +0300

    5/registration page (#12)
    * Добавить форму регистрации, стили для формы регистрации, добавил в .gitignore файлы с расширением .lock
    
    * 5: Редактирование формы регистрации
    изменение стилей
    внести исправления
    
    * 5: Завершить оформление формы
    регистрации
    Дополнить файл _default.sass - привязать футер
    
    * Изменить файл app.css после минификации

--- resources/views/pages/register_page.blade.php
+++ resources/views/pages/register_page.blade.php
@@ -1,42 +1,7 @@
 @extends('layout.index')
 
 @section('content')
-    <div class="container">
-        <div class="form-register px-6 py-4 max-w-6xl">
-            <form action="{{ route('register') }}" method="POST" class="grid grid-cols-1">
-                @csrf
-                <div class="form-register__wrapper px-6 py-4">
-                    @error('name')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    <input type="text" name="name" placeholder="name" class="h-8" value="{{old('name')}}">
-                </div>
 
-                <div class="form-register__wrapper px-6 py-4">
-                    @error('email')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    <input type="text" name="email" placeholder="e-mail адрес" class="h-8" value="{{old('email')}}">
-                </div>
-
-                <div class="form-register__wrapper px-6 py-4">
-                    @error('password')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    <input type="password" name="password" placeholder="пароль" class="h-8">
-                </div>
-
-                <div class="form-register__wrapper px-6 py-4">
-                    @error('password_confirmation')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    <input type="password" name="password_confirmation" placeholder="подтвердите пароль" class="h-8">
-                </div>
-
-                <div class="form-register__wrapper px-6 py-4">
-                    <button>зарегистрироваться</button>
-                </div>
-            </form>
-        </div>
-    </div>
+    @include('forms.register')
+    
 @endsection
