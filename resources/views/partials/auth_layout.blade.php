cfeb278bc58d5d2f4d83a6ae77c5f078bf74a565
Evgeny <puls2007@rambler.ru>
2022-03-15 00:24:47 +0500

    13: Адаптив для мобильных телефонов и планшетов.

--- resources/views/partials/auth_layout.blade.php
+++ resources/views/partials/auth_layout.blade.php
@@ -1,6 +1,6 @@
 <div class="header__personal">
     <a href="#" class="header__cart">
-        <img src="{{ asset('/images/svg/cart.svg') }}" alt="cart" width="50" height="50">
+        <img class="header__cart-image" src="{{ asset('/images/svg/cart.svg') }}" alt="cart" width="50" height="50">
     </a>
     <a @auth
             href="{{ route('logout') }}" class="header__auth header__auth_logout"
