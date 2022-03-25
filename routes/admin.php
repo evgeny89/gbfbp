481ec70b9e9815a0ee11373aa9e44dd7b6ea66b0
Evgeny <puls2007@rambler.ru>
2022-03-25 21:02:12 +0500

    17: Добавить роуты админки

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MaterialController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -7,9 +10,12 @@ use Illuminate\Support\Facades\Route;
 | Admin Routes
 |--------------------------------------------------------------------------
 */
-
-Route::get('/', function () {
-    return view('admin.index');
-})->name('admin-panel');
-
-
+Route::name('admin.')->group(function () {
+    Route::get('/', function () {return view('admin.index');})->name('home');
+    Route::get('/users', [UserController::class, 'list'])->name('users');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('user');
+    Route::get('/categories', [CategoryController::class, 'list'])->name('categories');
+    Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('category');
+    Route::get('/materials', [MaterialController::class, 'list'])->name('materials');
+    Route::get('/materials/{id}/edit', [MaterialController::class, 'edit'])->name('material');
+});
