8d022f6da9dfe9a7d9f75b4425636e2f4ae3eaf9
Evgeny <puls2007@rambler.ru>
2022-03-31 18:22:55 +0500

    17: Сгруппировать роуты и подправить шаблон меню

--- resources/views/admin/layout/base.blade.php
+++ resources/views/admin/layout/base.blade.php
@@ -6,9 +6,9 @@
             <aside class="col-4 aside">
                 <a href="{{ route('admin.home') }}">Hand Made Admin</a>
                 <div class="menu">
-                    <a href="{{ route('admin.users') }}" class="menu-link {{ request()->routeIs('admin.users') ? 'active' : '' }}">Пользователи</a>
-                    <a href="{{ route('admin.categories') }}" class="menu-link {{ request()->routeIs('admin.categories') ? 'active' : '' }}">Категории</a>
-                    <a href="{{ route('admin.materials') }}" class="menu-link {{ request()->routeIs('admin.materials') ? 'active' : '' }}">Материалы</a>
+                    <a href="{{ route('admin.users') }}" class="menu-link {{ request()->is('admin/users*') ? 'active' : '' }}">Пользователи</a>
+                    <a href="{{ route('admin.categories') }}" class="menu-link {{ request()->is('admin/categories*') ? 'active' : '' }}">Категории</a>
+                    <a href="{{ route('admin.materials') }}" class="menu-link {{ request()->is('admin/materials*') ? 'active' : '' }}">Материалы</a>
                 </div>
             </aside>
             <section class="col-8 workspace">
