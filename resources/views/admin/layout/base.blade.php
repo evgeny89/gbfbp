bf79cfe09c33a7708512a1971f44ea29852938a3
Evgeny <puls2007@rambler.ru>
2022-04-07 20:38:31 +0500

    17: Подправить шаблоны админки с учетом ширины вывода логов

--- resources/views/admin/layout/base.blade.php
+++ resources/views/admin/layout/base.blade.php
@@ -1,17 +1,19 @@
 @extends('admin.layout.index')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row wrapper">
-            <aside class="col-4 aside">
+            <aside class="col-2 aside">
                 <a href="{{ route('admin.home') }}">Hand Made Admin</a>
                 <div class="menu">
                     <a href="{{ route('admin.users') }}" class="menu-link {{ request()->is('admin/users*') ? 'active' : '' }}">Пользователи</a>
                     <a href="{{ route('admin.categories') }}" class="menu-link {{ request()->is('admin/categories*') ? 'active' : '' }}">Категории</a>
                     <a href="{{ route('admin.materials') }}" class="menu-link {{ request()->is('admin/materials*') ? 'active' : '' }}">Материалы</a>
+                    <a href="{{ route('admin.logs') }}" class="menu-link {{ request()->is('admin/logs*') ? 'active' : '' }}">Логи</a>
+                    @yield('log-files')
                 </div>
             </aside>
-            <section class="col-8 workspace">
+            <section class="col-10 workspace">
                 @yield('workspace')
             </section>
         </div>
