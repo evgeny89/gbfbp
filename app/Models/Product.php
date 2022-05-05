a3c8c3686ea979d680906b9f0d2f49f41babe8cb
Evgeny <puls2007@rambler.ru>
2022-05-05 23:26:27 +0500

    9: Добавить модели товара и фото товара с добавлением связей

---
+++ app/Models/Product.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'weight',
+        'description',
+        'shop_id',
+        'category_id',
+        'material_id',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | FUNCTION
+    |--------------------------------------------------------------------------
+    */
+    public function delete()
+    {
+        foreach ($this->images as $image) {
+            $image->deleteCurrentImages($image->file_name);
+        }
+
+        parent::delete();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductPhoto::class);
+    }
+
+    public function shop(): BelongsTo
+    {
+        return $this->belongsTo(Shop::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function material(): BelongsTo
+    {
+        return $this->belongsTo(Material::class);
+    }
+}
