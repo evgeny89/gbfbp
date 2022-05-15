51e98c20dd199168de6af7410182a44c29ca36c8
Evgeny <puls2007@rambler.ru>
2022-05-15 12:14:06 +0500

    9: Исправить стили и изменить работу компонента с изображениями

--- resources/js/Components/UserShop/ProductFormComponent.js
+++ resources/js/Components/UserShop/ProductFormComponent.js
@@ -4,16 +4,25 @@ import axios from "axios";
 function ProductFormComponent({product, close, routes, save, update}) {
     const [materials, setMaterials] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [category, setCategory] = useState(product.category_id);
+    const [material, setMaterial] = useState(product.material_id);
+    const [images, setImages] = useState([]);
+    const [removeImagesIds, setRemoveImagesIds] = useState([]);
+    const [imageName, setImageName] = useState("");
     const [err, setErr] = useState({})
     const form = useRef(null);
+    const file = useRef(null);
 
     const saveProduct = (e) => {
         e.preventDefault();
+        const fd = new FormData(form.current);
+        removeImagesIds.forEach(img => fd.append('delete_images[]', img));
+        images.forEach(img => fd.append('file_name[]', img));
         axios({
             method: "POST",
             url: getCurrentRoute(),
             headers: {"Content-Type": "multipart/form-data"},
-            data: new FormData(form.current)
+            data: fd
         })
             .then(r => {
                 if (product.id) {
@@ -23,7 +32,9 @@ function ProductFormComponent({product, close, routes, save, update}) {
                 }
                 close();
             })
-            .catch(e => console.log(e.message))
+            .catch(e => {
+                setErr(e.response.data.errors);
+            })
     };
 
     useEffect(() => {
@@ -43,6 +54,31 @@ function ProductFormComponent({product, close, routes, save, update}) {
         return product.id ? routes.updateItem.replace('=product=', product.id) : routes.createItem;
     }
 
+    const addImage = () => {
+        if (file.current.files.length) {
+            const image = file.current.files[0];
+            setImages([...images, image]);
+            file.current.value = "";
+            setImageName("")
+        }
+    }
+
+    const currentName = () => {
+        setImageName(file.current.files[0].name);
+    }
+
+    const removeFileImage = (name) => {
+        setImages([...images.filter(img => img.name !== name)]);
+    }
+
+    const removeServerImage = (imageId) => {
+        setRemoveImagesIds([...removeImagesIds, imageId])
+    }
+
+    const restoreServerImage = (imageId) => {
+        setRemoveImagesIds([...removeImagesIds.filter(i => i !== imageId)])
+    }
+
     return (
         <>
             <div className="form__backdrop"></div>
@@ -57,25 +93,26 @@ function ProductFormComponent({product, close, routes, save, update}) {
                             <div className="form__close" onClick={close}>x</div>
                         </div>
                         <div className="product__form-row">
-                            <label htmlFor="product_name">Наименование:</label>
+                            <label htmlFor="product_name" className={err?.name ? "product__form-row-title error" : "product__form-row-title"}>Наименование:</label>
                             <input defaultValue={product.name} id="product_name" name="name" type="text"
                                    className="product__data"/>
                         </div>
                         <div className="product__form-row">
-                            <label htmlFor="product_price">Цена:</label>
+                            <label htmlFor="product_price" className={err?.price ? "product__form-row-title error" : "product__form-row-title"}>Цена:</label>
                             <input defaultValue={product.price} id="product_price" name="price" type="text"
                                    className="product__data"/>
                         </div>
                         <div className="product__form-row">
-                            <label htmlFor="product_weight">Вес:</label>
+                            <label htmlFor="product_weight" className={err?.weight ? "product__form-row-title error" : "product__form-row-title"}>Вес:</label>
                             <input defaultValue={product.weight} id="product_weight" name="weight" type="text"
                                    className="product__data"/>
                         </div>
                         {
                             materials &&
                             <div className="product__form-row">
-                                <label htmlFor="product_material">Материал:</label>
-                                <select name="material_id" id="product_material" value={product.material_id}>
+                                <label htmlFor="product_material" className={err['material_id'] ? "product__form-row-title error" : "product__form-row-title"}>Материал:</label>
+                                <select name="material_id" id="product_material" value={material}
+                                        onChange={(e) => setMaterial(e.target.value)}>
                                     {materials.map(material => <option key={material.id} value={material.id}>{material.name}</option>)}
                                 </select>
                             </div>
@@ -83,38 +120,67 @@ function ProductFormComponent({product, close, routes, save, update}) {
                         {
                             categories &&
                             <div className="product__form-row">
-                                <label htmlFor="product_material">Категория:</label>
-                                <select name="category_id" id="product_material" value={product.category_id}>
+                                <label htmlFor="product_material" className={err['category_id'] ? "product__form-row-title error" : "product__form-row-title"}>Категория:</label>
+                                <select name="category_id" id="product_material" value={category}
+                                        onChange={(e) => setCategory(e.target.value)}>
                                     {categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)}
                                 </select>
                             </div>
                         }
                         <div className="product__form-column">
-                            <label htmlFor="product_description">Описание:</label>
+                            <label htmlFor="product_description" className={err?.description ? "product__form-row-title error" : "product__form-row-title"}>Описание:</label>
                             <textarea defaultValue={product.description} id="product_description"
                                       name="description"
                                       className="product__data" rows="8"/>
                         </div>
                         <div className="product__form-row">
-                            <label htmlFor="product_photo1">Фото:</label>
-                            <input id="product_photo1" name="file_name[]" type="file" className="product__data"/>
-                        </div>
-                        <div className="product__form-row">
-                            <label htmlFor="product_photo2">Фото:</label>
-                            <input id="product_photo2" name="file_name[]" type="file" className="product__data"/>
+                            <div>
+                                <label htmlFor="product_photo" className={err['file_name'] ? "product__form-row-title product_photo error" : "product__form-row-title product_photo"}>Добавить фото товара</label>
+                                <input ref={file} id="product_photo" type="file" className="image-product-input" onChange={currentName}/>
+                            </div>
+                            <div className="upload-wrapper">
+                                <span>{imageName}</span>
+                                {
+                                    imageName.length > 0 &&
+                                    <button className="upload-btn" type="button" onClick={addImage}>Загрузить</button>
+                                }
+                            </div>
                         </div>
                         <div className="product__form-row">
                             <div className="product__photos">
                                 {
                                     product.images &&
-                                    product.images.map((item) => {
+                                    product.images.map(item => {
                                             return (
-                                                <div className="photo__preview">
-                                                    <img key={item.id} src={item.small_image} alt={item.file_name}/>
+                                                <div className="photo__preview" key={item.id}>
+                                                    {
+                                                        removeImagesIds.includes(item.id)
+                                                            ? <button type="button" className="button__remove"
+                                                                      onClick={() => restoreServerImage(item.id)}>
+                                                                <span className="restore__icon"></span>
+                                                            </button>
+                                                            : <button type="button" className="button__remove"
+                                                                      onClick={() => removeServerImage(item.id)}>
+                                                                <span className="remove__icon"></span>
+                                                            </button>
+                                                    }
+                                                    <img className={removeImagesIds.includes(item.id) ? "server-image delete-mark" : "server-image"} src={item.small_image} alt={item.file_name} width="150" height="198"/>
                                                 </div>
                                             )
                                         }
                                     )
+                                }
+                                {
+                                    images && images.map(item => {
+                                        return (
+                                            <div className="photo__preview" key={item.name}>
+                                                <button type="button" className="button__remove" onClick={() => removeFileImage(item.name)}>
+                                                    <span className="remove__icon"></span>
+                                                </button>
+                                                <img src={URL.createObjectURL(item)} alt={item.name} width="150" height="198"/>
+                                            </div>
+                                        )
+                                    })
                                 }
                             </div>
                         </div>
