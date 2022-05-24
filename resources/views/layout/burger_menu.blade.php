f6feaa4d926478c4a8edf6d337f9ad180af41d13
Евгений <52931112+evgeny89@users.noreply.github.com>
2022-05-24 23:19:49 +0300

    33. Интеграция меню (#48)
    * 33. Интеграция меню
    
    * 33. Добавить роуты в меню
    
    * 33. fix. исправить работу категорий и материалов
    
    Co-authored-by: Evgeny <office.evgeny@yandex.ru>

--- resources/views/layout/burger_menu.blade.php
+++ resources/views/layout/burger_menu.blade.php
@@ -5,98 +5,15 @@
             <div class="menu__hide">
                 <button id="menu_hide" class="menu__hide-btn"></button>
             </div>
-            <div class="menu__item">
-                <a href="" class="item__link">
-                    <div class="item__icon">
-                        <img class="item__image" src="{{ asset('/images/menu_icons/accessories.svg') }}">
-                    </div>
-                    <div class="item__title">
-                        Аксесуары
-                    </div>
-                </a>
-            </div>
-            <div class="menu__item">
-                <a href="" class="item__link">
-                    <div class="item__icon">
-                        <img class="item__image" src="{{ asset('/images/menu_icons/clothes.svg') }}">
-                    </div>
-                    <div class="item__title">
-                        Одежда и обувь
-                    </div>
-                </a>
-            </div>
-            <div class="menu__item">
-                <a href="" class="item__link">
-                    <div class="item__icon">
-                        <img class="item__image" src="{{ asset('/images/menu_icons/interior.svg') }}">
-                    </div>
-                    <div class="item__title">
-                        Предметы интерьера
-                    </div>
-                </a>
-            </div>
-            <div class="menu__item">
-                <a href="" class="item__link">
-                    <div class="item__icon">
-                        <img class="item__image" src="{{ asset('/images/menu_icons/party.svg') }}">
-                    </div>
-                    <div class="item__title">
-                        Свадьбы и вечеринки
-                    </div>
-                </a>
-            </div>
-            <div class="menu__item">
-                <a href="" class="item__link link__active">
-                    <div class="item__icon">
-                        <img class="item__image" src="{{ asset('/images/menu_icons/toy.svg') }}">
-                    </div>
-                    <div class="item__title">
-                        Игрушки
-                    </div>
-                </a>
-                <div class="item__child">
-                    <a href="" class="item__link">
+            @foreach($categories as $category)
+                <div class="menu__item">
+                    <a href="{{ route('category_page', ['category' => $category->slug]) }}" class="item__link">
                         <div class="item__title">
-                            Для младенцев
-                        </div>
-                    </a>
-                    <a href="" class="item__link link__active">
-                        <div class="item__title">
-                            Мягкая игрушка
-                        </div>
-                    </a>
-                    <a href="" class="item__link">
-                        <div class="item__title">
-                            Куклы и фигурки
-                        </div>
-                    </a>
-                    <a href="" class="item__link">
-                        <div class="item__title">
-                            Головоломки
+                            {{ $category->name }}
                         </div>
                     </a>
                 </div>
-            </div>
-            <div class="menu__item">
-                <a href="" class="item__link">
-                    <div class="item__icon">
-                        <img class="item__image" src="{{ asset('/images/menu_icons/gift.svg') }}">
-                    </div>
-                    <div class="item__title">
-                        Подарки
-                    </div>
-                </a>
-            </div>
-            <div class="menu__item">
-                <a href="" class="item__link">
-                    <div class="item__icon">
-                        <img class="item__image" src="{{ asset('/images/menu_icons/materials.svg') }}">
-                    </div>
-                    <div class="item__title">
-                        Материалы для творчества
-                    </div>
-                </a>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
