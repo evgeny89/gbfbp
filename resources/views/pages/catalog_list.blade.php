b47f703f42a7fb78e9059c34e085e43887f6185e
good <good@good.ru>
2022-05-28 18:59:59 +0300

    36. верстка и стилизация списка категорий и материалов

--- resources/views/pages/catalog_list.blade.php
+++ resources/views/pages/catalog_list.blade.php
@@ -1,13 +1,25 @@
 @extends('layout.index')
 
 @section('content')
-    <section class="catalog">
+    <section class="catalog__list {{$type == 'category' ? 'catalog__category' : 'catalog__material'}}">
         <div class="container">
-            @foreach($entries as $entry)
-                <div>
-                    <a href="{{ route($route_name, [$type => $entry->slug]) }}">{{ $entry->name }}</a>
-                </div>
-            @endforeach
+            <div class="catalog__title">
+                {{$type == 'category' ? 'Категории' : 'Материалы'}}
+            </div>
+            <div class="catalog__list-wrapper">
+                @foreach($entries as $entry)
+                    <div class="catalog__item">
+                        <a class="item__link" href="{{ route($route_name, [$type => $entry->slug]) }}">
+                            <div class="item__image">
+                                <img src="{{ $entry->home }}" alt="">
+                            </div>
+                            <span class="link__text">
+                                {{ $entry->name }}
+                            </span>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
 @endsection
