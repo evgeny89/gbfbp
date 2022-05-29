b82e8047d194066aa4e64232b68fca1932d494c5
n1tb1 <31108065+n1tb1@users.noreply.github.com>
2022-05-29 19:11:31 +0300

    38/catalog page fixes (#58)
    * 37. Создание переиспользуемого шаблона секции каталога
    
    * 37. Интеграция шаблона секции каталога в шаблон страницы каталога
    
    * 37. Добавить данные категорий и материалов в метод отображения главной страницы
    
    * 37. Добавить данные категорий и материалов в шаблон главной страницы
    
    * 38. Исправить сортировку каталога вместо популярность сортируем по имени
    
    * 38. Рефакторинг сортировки компонента каталога
    
    * 38. Сборка проекта
    
    Co-authored-by: good <good@good.ru>

---
+++ resources/views/partials/catalog_list.blade.php
@@ -0,0 +1,21 @@
+<section class="catalog__list {{$type == 'category' ? 'catalog__category' : 'catalog__material'}}">
+    <div class="container">
+        <div class="catalog__title">
+            {{$type == 'category' ? 'Категории' : 'Материалы'}}
+        </div>
+        <div class="catalog__list-wrapper">
+            @foreach($entries as $entry)
+                <div class="catalog__item">
+                    <a class="item__link" href="{{ route($route_name, [$type => $entry->slug]) }}">
+                        <div class="item__image">
+                            <img src="{{ $entry->home }}" alt="">
+                        </div>
+                        <span class="link__text">
+                                {{ $entry->name }}
+                            </span>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
