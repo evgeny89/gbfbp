855987a79b5755b8136881694cb2bbc8cc31ae76
good <good@good.ru>
2022-05-29 17:37:47 +0300

    37. Интеграция шаблона секции каталога в шаблон страницы каталога

--- resources/views/pages/catalog_list.blade.php
+++ resources/views/pages/catalog_list.blade.php
@@ -1,25 +1,9 @@
 @extends('layout.index')
 
 @section('content')
-    <section class="catalog__list {{$type == 'category' ? 'catalog__category' : 'catalog__material'}}">
-        <div class="container">
-            <div class="catalog__title">
-                {{$type == 'category' ? 'Категории' : 'Материалы'}}
-            </div>
-            <div class="catalog__list-wrapper">
-                @foreach($entries as $entry)
-                    <div class="catalog__item">
-                        <a class="item__link" href="{{ route($route_name, [$type => $entry->slug]) }}">
-                            <div class="item__image">
-                                <img src="{{ $entry->home }}" alt="">
-                            </div>
-                            <span class="link__text">
-                                {{ $entry->name }}
-                            </span>
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </section>
+    @include('partials.catalog_list', [
+                'entries' => $entries,
+                'route_name' => $route_name,
+                'type' => $type
+            ])
 @endsection
